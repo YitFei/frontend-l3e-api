cc087e2bdfeb8419dd8f832bf551bd81e23b7297
Yit Fei <yifei0309@gmail.com>
2022-05-07 15:47:11 +0800

    master

--- src/pages/Subscriptions/Body.jsx
+++ src/pages/Subscriptions/Body.jsx
@@ -72,28 +72,56 @@ const allcourses = [
 const redirectToLink = "../CourseInfo";
 
 export default function Body() {
+  let URL_GetAllSubcriptionCourse =
+    "https://api.l3education.com.my/courseSubscription/list";
+
   let URL_GetAllCourseList = "https://api.l3education.com.my/course/list";
   const [courseList, setCourseList] = React.useState([]);
+  const [courseSubcriptionList, setCourseSubcriptionList] = React.useState([]);
   const [query, setQuery] = React.useState("");
   const [showAlert, setShowAlert] = React.useState(false);
-
   const [successAlert, setSuccessAlert] = React.useState(true);
+  const [refreshData, setRefreshData] = React.useState(false);
 
   React.useEffect(() => {
     GetAPI(URL_GetAllCourseList, false).then((res) => {
       setCourseList(res.data);
     });
-  }, []);
+
+    GetAPI(URL_GetAllSubcriptionCourse, false).then((res) => {
+      setCourseSubcriptionList(res.data);
+    });
+
+    if (refreshData) {
+      setRefreshData(false);
+    }
+  }, [refreshData]);
 
   React.useEffect(() => {
     setTimeout(() => {
       if (showAlert) {
+        setRefreshData(true);
         setShowAlert(false);
       }
     }, 1500);
   }, [showAlert]);
+
+  let CoursesHaventPurchased = courseList.filter((course, index) => {
+    return !courseSubcriptionList.find((filter) => {
+      return course.id === filter.courseId;
+    });
+  });
+
   return (
     <Box sx={{ flexGrow: 1, marginLeft: 1 }}>
+      {/* {console.log(
+        courseList.filter((course, index) => {
+          return !courseSubcriptionList.find((filter) => {
+            return course.id === filter.courseId;
+          });
+        })
+      )} */}
+
       {showAlert && (
         <Alert
           onClose={() => {
@@ -116,39 +144,35 @@ export default function Body() {
           </Item>
         </Grid>
 
-        {courseList
-          .filter((data) => {
-            if (query === "") {
-              //if query is empty
-              // console.log(data);
-              return data;
-            } else if (
-              data.courseName.toLowerCase().includes(query.toLowerCase())
-            ) {
-              //returns filtered array
-              return data;
-            }
-          })
-          .map((item, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item>
-                <Coursescard
-                  courseInfo={item}
-                  title={item.courseName}
-                  courseDescription={""}
-                  link={null}
-                  image={null}
-                  time={
-                    item.startTime.slice(0, 5) +
-                    " - " +
-                    item.endTime.slice(0, 5)
-                  }
-                  setShowAlert={setShowAlert}
-                  // redirectTo={redirectToLink}
-                />
-              </Item>
-            </Grid>
-          ))}
+        {CoursesHaventPurchased.filter((data) => {
+          if (query === "") {
+            //if query is empty
+            // console.log(data);
+            return data;
+          } else if (
+            data.courseName.toLowerCase().includes(query.toLowerCase())
+          ) {
+            //returns filtered array
+            return data;
+          }
+        }).map((item, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+            <Item>
+              <Coursescard
+                courseInfo={item}
+                title={item.courseName}
+                courseDescription={""}
+                link={null}
+                image={null}
+                time={
+                  item.startTime.slice(0, 5) + " - " + item.endTime.slice(0, 5)
+                }
+                setShowAlert={setShowAlert}
+                // redirectTo={redirectToLink}
+              />
+            </Item>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
